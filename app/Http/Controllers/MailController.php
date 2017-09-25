1dcbb086d6bae656fe8fd6a5b346b6752d6b19e1
heggies <heggies0iqbal@gmail.com>
2017-09-25 18:49:03 +0700

    apa lu liat2

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -14,27 +14,25 @@ class MailController extends Controller
     }
 
     public function send(Request $r) {
-        // return "wakwkakw";
-        // $base64 = $r->input('base64');
-        // $base64_str = substr($base64, strpos($base64, ",")+1);
-        // $image = base64_decode($base64_str);
-        // file_put_contents(url('images/testing.jpg'), $image);
+        $file_data = $r->input('base64'); 
+        $file_name = 'image_'.time().'.png'; //generating unique file name; 
+        @list($type, $file_data) = explode(';', $file_data);
+        @list(, $file_data) = explode(',', $file_data); 
+        if($file_data!=""){ // storing image in storage/app/public Folder 
+            $data = array('email' => 'heggies0iqbal@gmail.com', 
+                            'first_name' => 'Laporan Trafik Perusahaan X', 
+                            'from' => 'fkrfdllh@gmail.com',
+                            'from_name' => 'Fikri');
+
+            Mail::send('mail.mail', ['base64' => $file_data], function ($message) use ($data) {
+                $message->to($data['email'])
+                ->from($data['from'], $data['from_name'])
+                ->subject($data['first_name']);
+            });
+        }
+
         $response = [];
-        $response['base64'] = $r->all();
+        $response['message'] = "success";
         return response()->json($response);
-
-
-        $data = array('email' => 'fkrfdllh@gmail.com', 
-                        'first_name' => 'Laporan Trafik Perusahaan X', 
-                        'from' => 'fkrfdllh@gmail.com',
-                        'from_name' => 'Fikri');
-
-        Mail::send('mail.mail', ['base64' => $r->input('base64')], function ($message) use ($data) {
-            $message->to($data['email'])
-            ->from($data['from'], $data['from_name'])
-            ->subject($data['first_name']);
-        });
-
-        return $data;
     }
 }
